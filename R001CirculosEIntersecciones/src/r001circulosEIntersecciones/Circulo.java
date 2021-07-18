11d3b012564fbf22d6089f7c03a071c3c31b93a6
Cristian-Estevez <desarrolloyprog@gmail.com>
2021-07-18 11:16:38 -0300

    Terminado codigo de R001Circulos y circunferencias, Falta JUnit tests

--- R001CirculosEIntersecciones/src/r001circulosEIntersecciones/Circulo.java
+++ R001CirculosEIntersecciones/src/r001circulosEIntersecciones/Circulo.java
@@ -2,49 +2,52 @@ package r001circulosEIntersecciones;
 
 public class Circulo {
 	private Punto centro;
-	private double radio;
-	private double xMax, xMin, yMax, yMin;
-	
+	private double radio;	
 	
 	public Circulo(Punto cen, double r) {
 		centro = cen;
 		radio = r;
-		xMax = centro.getCoordX() + r;
-		xMin = centro.getCoordX() - r;
-		yMax = centro.getCoordY() + r;
-		yMin = centro.getCoordY() - r;
 	}
 	
-	public boolean intersectanEnX(Circulo c) {
-		if((this.xMax > c.xMin && this.xMax < c.xMax) || 
-				(this.xMin > c.xMin && this.xMin < c.xMax)) {
-			return true;
-		} 		
-		return false;
-	}
-	
-	public boolean intersectanEnY(Circulo c) {
-		if((this.yMax < c.yMax && this.yMax > c.yMin) ||
-				this.yMin > c.yMin && this.yMin < c.yMax) {
-			return true;
-		}
-		return false;
-	}
-	
-	public boolean intersectaCon(Circulo c) {
-		boolean enX = intersectanEnX(c);
-		boolean enY = intersectanEnY(c);
-		System.out.println(enX + "\n" + enY);
-		if (enX || enY) {
-			return true;
-		} 	
+	public double getDistanciaPuntos(Circulo c) {
+		double dist = 0;
+		double xDeA, xDeB, yDeA, yDeB;
+		xDeA = this.centro.getCoordX();
+		xDeB = c.centro.getCoordX();
+		yDeA = this.centro.getCoordY();
+		yDeB = c.centro.getCoordY();
 		
-		return false;
+		dist = Math.sqrt((xDeA - xDeB) * (xDeA - xDeB) + (yDeA - yDeB) * (yDeA - yDeB));
+		// System.out.println(dist); for debug
+		return dist;
+	}
+	
+	public double getSumaRadios(Circulo c) {
+		double sumaRadios;
+		sumaRadios = this.radio + c.radio;
+		// System.out.println(sumaRadios); for debug
+		return sumaRadios;
+	}
+		
+	public boolean intersectaCon(Circulo c) {
+		double distanciaCentros = getDistanciaPuntos(c);
+		double sumaRadios = getSumaRadios(c);
+		
+		if (distanciaCentros > sumaRadios) {
+			return false;
+		} 
+		return true;
 	}
 	
 	public static void main(String[] args) {
 		Circulo c1 = new Circulo(new Punto(0, 0), 1);
 		Circulo c2 = new Circulo(new Punto(1, 1), 1.5);
 		System.out.println(c1.intersectaCon(c2)); // true
-	}
+	} 
 }
+
+/* FUNDAMENTOS TEORICOS EN: 
+ * https://www.geeksforgeeks.org/check-two-given-circles-touch-intersect/
+ * https://mathworld.wolfram.com/Circle-CircleIntersection.html
+ * http://mimosa.pntic.mec.es/clobo/geoweb/circun3.htm 
+*/
