4ab17393b21423c94c6305c231b6e3aa007070cc
Cristian-Estevez <cuentacursosprogramacion@gmail.com>
2021-07-20 15:20:48 -0300

    haciendo cuentaCifras

--- CuentaCifras/src/cuentaCifras/CuentaCifras.java
+++ CuentaCifras/src/cuentaCifras/CuentaCifras.java
@@ -2,9 +2,28 @@ package cuentaCifras;
 
 public class CuentaCifras {
 
+	static public int contar(int num){
+		int resultado = 0;
+		
+		
+		
+		int temp = num;
+		do {
+			resultado += temp % 10;
+			temp = temp / 10;
+		} while (temp != 0);
+		if (resultado < 10) {
+			return resultado;
+		}
+		contar(resultado);
+		
+		return 1;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(CuentaCifras.contar(2334));
+		
 	}
 
 }
